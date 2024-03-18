5b4e89714dbd8c0aa567e4af44e8f1451667c7c4
Cintia Munoz Vales <cintiamuva@gmail.com>
2024-03-18 11:47:15 -0300

    Iniciando evaluación integradora M2

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+* @author Cintia Muñoz Valdés
+* @version 1.0
+* */
+public class Main {
+    //Método principal dónde se ejecuta la aplicación
+    public static void main(String[] args) {
+
+    }
+}
+
